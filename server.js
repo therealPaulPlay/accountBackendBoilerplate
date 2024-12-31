e0fb9a20fe1475d43e19d5d9bc807a0d762872bb
PaulPlay <80539587+therealPaulPlay@users.noreply.github.com>
2024-12-31 18:21:41 +0100

    fix

--- server.js
+++ server.js
@@ -242,7 +242,7 @@ app.post('/accounts/register', registerLimiter, async (req, res) => {
         const now = new Date();
 
         // Insert new user into the database
-        const insertUserQuery = 'INSERT INTO accounts (user_name, email, password, created_at) VALUES (?, ?, ?, ?, ?)';
+        const insertUserQuery = 'INSERT INTO accounts (user_name, email, password, created_at) VALUES (?, ?, ?, ?)';
         const newUser = await new Promise((resolve, reject) => {
             db.query(insertUserQuery, [userName, email, hashedPassword, now], (err, results) => {
                 if (err) return reject(err);
